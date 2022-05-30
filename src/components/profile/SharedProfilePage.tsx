95e129e04b71d4f291613f93505a7f717576a58f
bodo6778 <darkofbobo@gmail.com>
2022-05-30 22:25:09 +0300

    build error

--- src/components/profile/SharedProfilePage.tsx
+++ src/components/profile/SharedProfilePage.tsx
@@ -14,7 +14,8 @@ const SharedProfilePage: React.FC<SharedProfilePageProps> = ({ user }) => {
     <Box>
       {!token && (
         <Text fontSize="xl" mb={6}>
-          This is <b>{user.name}</b>&apos;s profile. Take a look at it's wishes:
+          This is <b>{user.name}</b>&apos;s profile. Take a look at it&apos;s
+          wishes:
         </Text>
       )}
       <Wishlist wishlists={user.wishlists} />
